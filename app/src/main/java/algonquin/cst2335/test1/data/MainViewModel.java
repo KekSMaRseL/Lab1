abe7a92090cdfaaeaa06579b1137aabf19a2f64b
Nikita Senkov <senk0014@algonquinlive.com>
2023-05-31 19:36:22 -0400

    PICTURES

--- app/src/main/java/algonquin/cst2335/test1/data/MainViewModel.java
+++ app/src/main/java/algonquin/cst2335/test1/data/MainViewModel.java
@@ -1,11 +1,14 @@
 package algonquin.cst2335.test1.data;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
 
-public String theText = "Hello World!";
-public String bText = "Click me!";
-public String EditString;
+
+    public MutableLiveData<String> theText = new MutableLiveData<>();
+    public MutableLiveData<Boolean> isChecked = new MutableLiveData<>();
 
 }
+
+
--- app/src/main/java/algonquin/cst2335/test1/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/test1/ui/MainActivity.java
@@ -1,72 +1,58 @@
 package algonquin.cst2335.test1.ui;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.Switch;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
 import algonquin.cst2335.test1.R;
 import algonquin.cst2335.test1.data.MainViewModel;
 import algonquin.cst2335.test1.databinding.ActivityMainBinding;
 
-
 public class MainActivity extends AppCompatActivity {
+
     private ActivityMainBinding variableBinding;
     private MainViewModel model;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         model = new ViewModelProvider(this).get(MainViewModel.class);
-
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot());
 
-
-        TextView theText =  variableBinding.theText;         //findViewById( R.id.theText);
-        Button butt = variableBinding.theButton;                 //findViewById(R.id.theButton);
-        EditText theEdit = variableBinding.theEditText;                              //findViewById( R.id.theEditText);
-        CheckBox myCheckbox = variableBinding.myCheckbox;
-        Switch mySwitch = variableBinding.mySwitch;
-        RadioButton myRadioButton = variableBinding.myRadioButton;
-
-        myCheckbox.setOnCheckedChangeListener( (a, b) -> {
-            theText.setText("The checkbox is on?" + b);
+        variableBinding.myCheckbox.setOnCheckedChangeListener((a, b) -> {
+            model.isChecked.postValue(b);
         });
 
-        mySwitch.setOnCheckedChangeListener( (a, b) -> {
-            theText.setText("The switch is on?" + b);
+        variableBinding.mySwitch.setOnCheckedChangeListener((a, b) -> {
+            model.isChecked.postValue(b);
         });
 
-        myRadioButton.setOnCheckedChangeListener( (a, b) -> {
-            theText.setText("The Radio Button is on?" + b);
+        variableBinding.myRadioButton.setOnCheckedChangeListener((a, b) -> {
+            model.isChecked.postValue(b);
         });
 
+        model.isChecked.observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                variableBinding.myCheckbox.setChecked(aBoolean);
+                variableBinding.mySwitch.setChecked(aBoolean);
+                variableBinding.myRadioButton.setChecked(aBoolean);
+                Toast.makeText(MainActivity.this, "The value is now: " + aBoolean, Toast.LENGTH_SHORT).show();
+            }
+        });
 
-        theText.setText(model.theText);
-        butt.setText(model.bText);
-        theEdit.setText(model.EditString);
-        variableBinding.theText.setText(model.EditString);
-        variableBinding.theButton.setOnClickListener(click ->
-                {
-                    model.EditString = variableBinding.theEditText.getText().toString();
-                    variableBinding.theText.setText(model.EditString);
-                }
+        model.theText.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                variableBinding.theText.setText(s);
+            }
+        });
 
-
-
-
-                );
+        variableBinding.theButton.setOnClickListener(click -> {
+            model.theText.postValue(variableBinding.theEditText.getText().toString());
+        });
 
         variableBinding.myImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,40 +63,5 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
             }
         });
-
-
-        //b.setOnClickListener(  (v) -> {
-            //model.theText = "You clicked the button";
-            //model.bText = "something new here";
-            //model.theEditText = theEdit.getText().toString();
-
-            //theText.setText(model.theText);
-            //b.setText(model.bText);
-            //theText.setText("Your edit ext has: " + model.theEditText);
-
-
-
-            //String words = theEdit.getText().toString();
-            //theText.setText(words);
-       // }
-         //);
-
-        //b.setOnClickListener(new View.OnClickListener() {
-           // @Override
-           // public void onClick(View v) {
-
-               //String words = theEdit.getText().toString();
-                //theText.setText(words);
-            //}
-        //});
-
-
-
     }
 }
-
-
-
-
-
-
